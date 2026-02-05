8351147bbf21d19b78b7819c0fe6a10abd3e1505
Duong Anh Hao <haor06480@gmail.com>
2026-02-05 17:41:03 +0700

    nop bai lab 5

--- bai2.c
+++ bai2.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+  * Họ và tên: [Dương Anh Hào]
+ * MSSV:      [PS44395]
+ * Lớp:       [CS21302]
  *****************************************************************************/
 
 //  BÀI 2: XÂY DỰNG HÀM TÍNH NĂM NHUẬN 
@@ -12,12 +12,28 @@
  
 #include <stdio.h>
 
-//Tạo hàm 
-
-int main() {
-
-    // //Gọi hàm trong hàm main 
-    
-    return 0;
+// Tạo hàm kiểm tra năm nhuận
+int laNamNhuan(int nam) {
+    if (nam % 400 == 0)
+        return 1;
+    else if (nam % 4 == 0 && nam % 100 != 0)
+        return 1;
+    else
+        return 0;
 }
 
+int main() {
+    int nam;
+
+    // Nhập năm
+    printf("Nhap nam: ");
+    scanf("%d", &nam);
+
+    // Gọi hàm kiểm tra
+    if (laNamNhuan(nam) == 1)
+        printf("%d la nam nhuan", nam);
+    else
+        printf("%d khong phai la nam nhuan", nam);
+
+    return 0;
+}
